0fda966886d25b51ddb5c2755c8824cb8a98a6a2
deprudentstore <rasheedlukmanayomide@gmail.com>
2026-08-07 15:30:18 +0100

    Add temporary debug route to seed database

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Illuminate\Support\Facades\Artisan;
+Route::get('/seed-db-xk92', function () {
+    if (request()->query('key') !== 'buildmart-seed-2026') {
+        abort(404);
+    }
+    Artisan::call('db:seed', ['--force' => true]);
+    return response()->json(['status' => 'seeded', 'output' => Artisan::output()]);
+});
